8e23120ede4fde5e1ec7d596697fd9892d23e0f8
KatsaWins <chr.katsaros99@gmail.com>
2026-05-15 14:01:08 +0300

    Add luxury page transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
+import { AnimatePresence } from 'framer-motion';
+
 import ScrollToTop from './components/ScrollToTop';
+import PageTransition from './components/PageTransition';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import FourthSection from './components/FourthSection';
 
 import Home from './components/Home';
 import About from './components/About';
@@ -15,7 +19,7 @@ import Experiences from './components/Experiences';
 
 import AgeWarning from './components/AgeWarning';
 import Underage from './components/Underage';
-import FourthSection from './components/FourthSection';
+
 function AppContent() {
   const location = useLocation();
 
@@ -23,25 +27,84 @@ function AppContent() {
     <>
       <ScrollToTop />
 
-      {/* Hide age warning on underage page */}
+      {/* AGE WARNING */}
       {location.pathname !== '/underage' && <AgeWarning />}
 
-      {/* Hide navbar/footer on underage page */}
+      {/* NAVBAR */}
       {location.pathname !== '/underage' && <Navbar />}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/experiences" element={<Experiences />} />
-        <Route path="/protocol" element={<Protocol />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/application" element={<Application />} />
+      {/* PAGE TRANSITIONS */}
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <PageTransition>
+                <Home />
+              </PageTransition>
+            }
+          />
 
-        {/* Underage page */}
-        <Route path="/underage" element={<Underage />} />
-      </Routes>
+          <Route
+            path="/about"
+            element={
+              <PageTransition>
+                <About />
+              </PageTransition>
+            }
+          />
 
+          <Route
+            path="/experiences"
+            element={
+              <PageTransition>
+                <Experiences />
+              </PageTransition>
+            }
+          />
+
+          <Route
+            path="/protocol"
+            element={
+              <PageTransition>
+                <Protocol />
+              </PageTransition>
+            }
+          />
+
+          <Route
+            path="/gallery"
+            element={
+              <PageTransition>
+                <Gallery />
+              </PageTransition>
+            }
+          />
+
+          <Route
+            path="/application"
+            element={
+              <PageTransition>
+                <Application />
+              </PageTransition>
+            }
+          />
+
+          <Route
+            path="/underage"
+            element={
+              <PageTransition>
+                <Underage />
+              </PageTransition>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
+
+      {/* FOOTER CTA */}
       {location.pathname !== '/underage' && <FourthSection />}
+
+      {/* FOOTER */}
       {location.pathname !== '/underage' && <Footer />}
     </>
   );
